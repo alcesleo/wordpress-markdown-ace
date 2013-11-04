3c43f560b9356ece129d3a7ef11918f69b1812b1
alcesleo <lagginglion@gmail.com>
2013-11-04 21:31:44 +0100

    Functional version of the plugin! Still needs some tweaks.

--- wp-markdown-ace.php
+++ wp-markdown-ace.php
@@ -11,14 +11,51 @@
 
 class WP_Markdown_Ace {
 
+	private static $PLUGIN_URL;
+
 	public function __construct() {
-		add_action( 'admin_init', array( $this, 'on_wp_admin_init' ) );
+
+		// TODO: More dynamic
+		self::$PLUGIN_URL = WP_PLUGIN_URL.'/wp-ace';
+
+		add_action( 'admin_init', array( $this, 'on_admin_init' ) );
 	}
 
-	public function on_wp_admin_init() {
+	/**
+	 * Takes action on correct admin-pages
+	 */
+	public function on_admin_init() {
 		if ( $this->editing_post() ) {
-			print 'BOOYAH';
+			add_action( 'admin_enqueue_scripts', array( $this, 'on_admin_enqueue_scripts' ) );
 		}
+	}
+
+	/**
+	 * Enques the javascripts
+	 */
+	public function on_admin_enqueue_scripts() {
+		// custom styles
+		wp_register_style( 'ace_editor_css', self::$PLUGIN_URL.'/css/textarea.css', false );
+		wp_enqueue_style( 'ace_editor_css' );
+
+		// ace editor
+		wp_register_script( 'ace_editor', self::$PLUGIN_URL.'/js/ace-builds/src/ace.js', false );
+		wp_enqueue_script( 'ace_editor' );
+
+		// ace markdown mode
+		wp_register_script( 'ace_mode_markdown', self::$PLUGIN_URL.'/js/ace-builds/src/mode-markdown.js', array( 'ace_editor' ) );
+		wp_enqueue_script( 'ace_mode_markdown' );
+
+		// initialization in footer
+		wp_register_script( 'ace_replace_textarea', self::$PLUGIN_URL.'/js/replace-textarea.js', array( 'ace_editor' ), true );
+		wp_enqueue_script( 'ace_replace_textarea' );
+	}
+
+	/**
+	 * Replaces the editor with ace
+	 */
+	public function modify_textarea() {
+
 	}
 
 	/**
@@ -30,4 +67,5 @@ class WP_Markdown_Ace {
 	}
 }
 
+// Instanciate the plugin
 $wp_markdown_ace = new WP_Markdown_Ace();
