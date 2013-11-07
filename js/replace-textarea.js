856028283cdbb51d32271ee3ebf8bd9d36ecd825
alcesleo <lagginglion@gmail.com>
2013-11-07 16:38:18 +0100

    Rename vars

--- js/replace-textarea.js
+++ js/replace-textarea.js
@@ -4,22 +4,23 @@
         // insert #wp_ace_editor div after textarea
         $('textarea.wp-editor-area').after('<div id="wp_ace_editor"></div>');
 
-        var styles_textarea = $('textarea.wp-editor-area').hide();
-        var styles_editor = ace.edit("wp_ace_editor");
+        // hide textarea and get elements
+        var textarea = $('textarea.wp-editor-area').hide();
+        var aceEditor = ace.edit("wp_ace_editor");
 
-        styles_editor.setTheme("ace/theme/chrome");
-        styles_editor.setShowPrintMargin(false);
+        // ace settings
+        aceEditor.setTheme("ace/theme/chrome");
+        aceEditor.setShowPrintMargin(false);
 
         var MarkdownMode = require("ace/mode/markdown").Mode;
-        styles_editor.getSession().setMode(new MarkdownMode());
+        aceEditor.getSession().setMode(new MarkdownMode());
 
         // connect editors with textareas
         // http://stackoverflow.com/questions/6440439/how-do-i-make-a-textarea-an-ace-editor
-        styles_editor.getSession().setValue(styles_textarea.val());
+        aceEditor.getSession().setValue(textarea.val());
 
-        // connect the Ace instance to the hidden textarea
-        styles_editor.getSession().on('change', function() {
-            styles_textarea.val(styles_editor.getSession().getValue());
+        aceEditor.getSession().on('change', function() {
+            textarea.val(aceEditor.getSession().getValue());
         });
 
     });
