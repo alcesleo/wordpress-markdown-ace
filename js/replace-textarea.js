6cf97b1ec7499394ec81efdd9eab85472d45dddc
alcesleo <lagginglion@gmail.com>
2013-11-13 16:34:11 +0100

    Adding $-signs to jQuery object vars

--- js/replace-textarea.js
+++ js/replace-textarea.js
@@ -5,22 +5,24 @@
         $('textarea.wp-editor-area').after('<div id="wp_ace_editor"></div>');
 
         // hide textarea and get elements
-        var textarea = $('textarea.wp-editor-area').hide();
-        var aceEditor = ace.edit("wp_ace_editor");
+        var $textarea = $('textarea.wp-editor-area').hide();
+        var $aceEditor = ace.edit("wp_ace_editor");
+
+        $textarea.hide();
 
         // ace settings
-        aceEditor.setTheme("ace/theme/chrome");
-        aceEditor.setShowPrintMargin(false);
+        $aceEditor.setTheme("ace/theme/chrome");
+        $aceEditor.setShowPrintMargin(false);
 
         var MarkdownMode = require("ace/mode/markdown").Mode;
-        aceEditor.getSession().setMode(new MarkdownMode());
+        $aceEditor.getSession().setMode(new MarkdownMode());
 
         // connect editors with textareas
         // http://stackoverflow.com/questions/6440439/how-do-i-make-a-textarea-an-ace-editor
-        aceEditor.getSession().setValue(textarea.val());
+        $aceEditor.getSession().setValue($textarea.val());
 
-        aceEditor.getSession().on('change', function() {
-            textarea.val(aceEditor.getSession().getValue());
+        $aceEditor.getSession().on('change', function() {
+            $textarea.val($aceEditor.getSession().getValue());
         });
 
     });
